a60ad24ffbc87dec97282830926327ff08edde01
aaaa <dilshodovziyodillo@gmail.com>
2023-09-27 12:52:57 +0500

    Universal-Path commit

---
+++ src/ChronoScholar.Domain/Configurations/DBPath/DatabasePath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoScholar.Domain.Configurations.DBPath
+{
+    public static class DatabasePath
+    {
+        public static string UserDb = "../../../../ChronoScholar.Data/Databases/UserDb.txt";
+        public static string SellerDb = "../../../../ChronoScholar.Data/Databases/SellerDb.txt";
+        public static string TextbookDb = "../../../../ChronoScholar.Data/Databases/TextbookDb.txt";
+    }
+}
